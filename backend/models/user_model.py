dc33599ac3072628e6595ed18787116d51c0cfb9
isurudayananda <129027427+isurudayananda@users.noreply.github.com>
2024-11-03 09:41:21 +0530

    Update user_model.py

--- backend/models/user_model.py
+++ backend/models/user_model.py
@@ -1,12 +1,35 @@
-from pydantic import BaseModel
+from bson import ObjectId
 
+class User:
+    def __init__(self, _id=None, username=None, password=None, email=None, gender=None, age=None, interested_categories=None):
+        self._id = _id
+        self.username = username
+        self.password = password
+        self.email = email
+        self.gender = gender
+        self.age = age
+        self.interested_categories = interested_categories
 
-class User(BaseModel):
-    id: int
-    username: str | None = None
-    email: str | None = None
-    is_admin: bool | None = None
+    def to_dict(self):
+        return {
+            "_id": self._id,
+            "username": self.username,
+            "password": self.password,
+            "email": self.email,
+            "gender": self.gender,
+            "age": self.age,
+            "interested_categories": self.interested_categories
+        }
 
-
-class UserInDB(User):
-    hashed_password: str
+    @classmethod
+    def from_dict(cls, data):
+        return cls(
+            _id=data["_id"],
+            username=data["username"],
+            password=data["password"],
+            email=data["email"],
+            gender=data["gender"],
+            age=data["age"],
+            interested_categories=data["interested_categories"]
+        )
+        
