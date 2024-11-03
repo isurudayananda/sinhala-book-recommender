9e43df1114dab2899bc5237776b73644b3c97bd5
isurudayananda <129027427+isurudayananda@users.noreply.github.com>
2024-11-04 04:18:28 +0530

    get recommendations auth works

--- backend/auth/authorize.py
+++ backend/auth/authorize.py
@@ -36,10 +36,10 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
         username: str = payload.get("sub")
         if username is None:
             raise credentials_exception
-        token_data = TokenData(username=username)
+        print(username)
     except JWTError:
         raise credentials_exception
-    user = get_user(username=token_data.username)
+    user = get_user(username=username)
     if user is None:
         raise credentials_exception
     return user
--- backend/routes/books.py
+++ backend/routes/books.py
@@ -1,14 +1,27 @@
-from fastapi import APIRouter, Form
+from fastapi import APIRouter, Depends, Form
 
 from services.book_info_service import get_info
 from services.books_service import get_recommendations
 from services.similar_books_service import get_similar_books
+from auth.authorize import get_current_user, oauth2_scheme, credentials_exception
 
 router = APIRouter(
     prefix="/api/books",
     tags=["books"],
     responses={404: {"description": "The requested uri was not found"}},
 )
+
+
+@router.get("/get-recommendations")
+async def get_recommend_auth(
+    token: str = Depends(oauth2_scheme)
+):
+    if await get_current_user(token) is None:
+        raise credentials_exception
+    
+    current_user = await get_current_user(token)
+
+    return get_recommendations(current_user.age, current_user.gender)
 
 
 @router.post("/get-recommendations")
