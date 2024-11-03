8776981d72b7be4b5c685fdd632b56014be2e5cb
isurudayananda <129027427+isurudayananda@users.noreply.github.com>
2024-11-04 04:42:46 +0530

    img_url added

--- backend/models/book_model.py
+++ backend/models/book_model.py
@@ -1,13 +1,14 @@
 from bson import ObjectId
 
 class Book:
-    def __init__(self, _id=None, book_name=None, isbn=None, author=None, url=None, categories=None):
+    def __init__(self, _id=None, book_name=None, isbn=None, author=None, url=None, categories=None, img_url=None):
         self._id = ObjectId(_id) if _id else None
         self.book_name = book_name
         self.isbn = isbn
         self.author = author
         self.url = url
         self.categories = categories
+        self.img_url = img_url
 
     def to_dict(self):
         return {
@@ -16,7 +17,8 @@ class Book:
             "ISBN": self.isbn,
             "Author": self.author,
             "URL": self.url,
-            "Category": self.categories
+            "Category": self.categories,
+            "img_url": self.img_url
         }
 
     @classmethod
@@ -27,5 +29,6 @@ class Book:
             isbn=data.get("ISBN"),
             author=data.get("Author"),
             url=data.get("URL"),
-            categories=data.get("Category")
+            categories=data.get("Category"),
+            img_url=data.get("img_url")
         )
