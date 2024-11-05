44973d3a35c0da3a4e978994c23632be1626560a
isurudayananda <129027427+isurudayananda@users.noreply.github.com>
2024-11-05 07:48:59 +0530

    3 book recommender

--- backend/services/books_service.py
+++ backend/services/books_service.py
@@ -9,17 +9,36 @@ book_name_encoder = joblib.load('bin\\knn\\book_name_encoder.joblib')
 
 
 # Book recommendation function
-def recommend_book(age, gender):
+def recommend_book_single(age, gender):
     # Encode inputs to match training data encoding
     gender_encoded = gender_encoder.transform([gender])[0]
     # Prepare input for KNN
     input_data = pd.DataFrame([[age, gender_encoded]], columns=['age', 'gender'])
     input_scaled = scaler.transform(input_data)
+    
     # Predict the book name
     book_name_encoded = knn.predict(input_scaled)
     book_name = book_name_encoder.inverse_transform(book_name_encoded)
     return book_name
 
 
+def recommend_books(age, gender, num_recommendations=5):
+    # Encode inputs to match training data encoding
+    gender_encoded = gender_encoder.transform([gender])[0]
+    # Prepare input for KNN
+    input_data = pd.DataFrame([[age, gender_encoded]], columns=['age', 'gender'])
+    input_scaled = scaler.transform(input_data)
+    
+    # Get the indices of the nearest neighbors
+    distances, indices = knn.kneighbors(input_scaled, n_neighbors=num_recommendations)
+    # Decode the recommended book names
+    recommended_books_encoded = knn._y[indices[0]]
+    recommended_books = book_name_encoder.inverse_transform(recommended_books_encoded)
+    
+    print(recommended_books[0] + " / " + recommended_books[1]+ " / "  + recommended_books[2])
+    
+    return recommended_books
+
+
 def get_recommendations(age: int, gender: str):
-    return recommend_book(age, gender)
+    return recommend_books(age, gender)
